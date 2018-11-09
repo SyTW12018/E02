7437401b66276e7ab660ff28098d0278102678c3
Eduardo Flores <eduflores@MacBook-Pro-de-Eduardo.local>
2018-11-09 20:00:39 +0000

    Añadido componentes y rutas del proyecto

--- DIYHacks/src/app/app-routing.module.ts
+++ DIYHacks/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ExploreComponent } from './components/explore/explore.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component:LoginComponent},
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'profile/:id', component:ProfileComponent},
+  {path: 'explore', component:ExploreComponent},
+  {path: '**', pathMatch:'full', redirectTo:'dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- DIYHacks/src/app/app.module.ts
+++ DIYHacks/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ExploreComponent } from './components/explore/explore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProfileComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
