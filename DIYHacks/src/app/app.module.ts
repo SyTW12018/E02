e10e44b516ae0aed2f10aa678b9e89a0623f6a23
Eduardo Flores <eduflores@MacBook-Pro-de-Eduardo.local>
2018-11-29 11:54:27 +0000

    Añadiendo servicios a nuestro proyecto

--- DIYHacks/src/app/app.module.ts
+++ DIYHacks/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +13,10 @@ import { ExploreComponent } from './components/explore/explore.component';
 
 import { UsersService } from './services/users.service';
 import { SignupComponent } from './components/signup/signup.component';
+import { AuthenticationService } from './services/authentication.service';
+import { AuthguardService } from './services/authguard.service';
+import { ErrorInterceptorService } from './services/error-interceptor.service'
+import { JwtInterceptorService } from './services/jwt-interceptor.service'
 
 @NgModule({
   declarations: [
@@ -32,7 +36,11 @@ import { SignupComponent } from './components/signup/signup.component';
     ReactiveFormsModule
   ],
   providers: [
-    UsersService
+    UsersService,
+    AuthguardService,
+    AuthenticationService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true }
   ],
   bootstrap: [AppComponent]
 })
--- DIYHacks/src/app/components/login/login.component.ts
+++ DIYHacks/src/app/components/login/login.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { UsersService } from '../../services/users.service'
 import { User } from '../../models/user.model';
+import { first } from 'rxjs/operators';
+
+import { AuthenticationService } from '../../services/authentication.service';
+
 
 @Component({
   selector: 'app-login',
@@ -12,11 +16,12 @@ import { User } from '../../models/user.model';
 
   `]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   formulario:FormGroup;
 
-  constructor(private userService:UsersService) {
+  constructor(private userService:UsersService,
+              private authenticationService: AuthenticationService) {
 
     this.formulario = new FormGroup({
       'user':     new FormControl(  ''   , [
@@ -31,11 +36,29 @@ export class LoginComponent {
     })
 
   }
+  ngOnInit() {
+  // reset login status
+  this.authenticationService.logout();
+  // get return url from route parameters or default to '/'
+  // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
   // login
   login() {
     console.log('Login')
     console.log(this.formulario)
     console.log(this.formulario.value)
+
+    this.authenticationService.login('username', 'password')
+           .pipe(first())
+           .subscribe(
+               data => {
+                 // Navegar a la página solicitada
+                  // this.router.navigate([this.returnUrl]);
+               },
+               error => {
+                //Si las credenciales no son válidas...
+
+               });
   }
 
 
